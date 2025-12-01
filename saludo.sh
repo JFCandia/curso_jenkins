e2437c206a437a01dc05d58f631d4d9ebadd50be
JFCandia <candia1978@gmail.com>
2025-12-01 14:44:14 -0300

    Prueba 2 Juan Candia

--- saludo.sh
+++ saludo.sh
@@ -1,3 +1,3 @@
 echo "Hola mundo con jenkins y github"
 echo "Probando trigger desde github y jenkins"
-echo "Probando trigger desde github y usando grok"
+echo "Probando trigger desde github y jenkins Juan Francisco Candia"
