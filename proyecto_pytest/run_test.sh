882b6f31452557d044f41bfaed7948701d814df3
JFCandia <candia1978@gmail.com>
2025-12-13 18:28:15 -0300

    Agregar carpeta informes

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 echo "activando el entorno virtual"
 
-# Crear venv si no existe
 if [ ! -d "venv" ]; then
     python3 -m venv venv
 fi
-
-# Activar venv
 source venv/bin/activate
 
 echo "instalando las dependencias"
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r requisitos.txt
+
+echo "creando carpeta de reportes si no existe"
+mkdir -p informes
 
 echo "ejecutando las pruebas con pytest"
-pytest test/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
+pytest prueba/ --junitxml=informes/test-results.xml --html=informes/test-results.html --self-contained-html
 
-echo "pruebas completadas resultados en reports/"
+echo "pruebas completadas resultados en informes/"
