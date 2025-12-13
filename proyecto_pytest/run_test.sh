6eac718074cb558d3600413417a27a199bf409fb
JFCandia <candia1978@gmail.com>
2025-12-13 18:24:45 -0300

    Probando test_run nueva actualizacion

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 echo "activando el entorno virtual"
+
+# Crear venv si no existe
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+# Activar venv
 source venv/bin/activate
 
 echo "instalando las dependencias"
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "ejecutando las pruebas con pytest"
