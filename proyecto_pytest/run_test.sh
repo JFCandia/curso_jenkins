888a9eb38783dd35d54610af99630d3ed42b49a5
JFCandia <candia1978@gmail.com>
2025-12-13 18:30:49 -0300

    modificando carpeta informes

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -8,12 +8,12 @@ source venv/bin/activate
 
 echo "instalando las dependencias"
 pip install --upgrade pip
-pip install -r requisitos.txt
+pip install -r requirements.txt
 
 echo "creando carpeta de reportes si no existe"
 mkdir -p informes
 
 echo "ejecutando las pruebas con pytest"
-pytest prueba/ --junitxml=informes/test-results.xml --html=informes/test-results.html --self-contained-html
+pytest test/ --junitxml=informes/test-results.xml --html=informes/test-results.html --self-contained-html
 
 echo "pruebas completadas resultados en informes/"
