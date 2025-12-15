90ad73cc96e7c0b2ba1cc28263d297cf1b3ac01c
JFCandia <candia1978@gmail.com>
2025-12-15 00:22:20 -0300

    mejorando css de reporte

--- proyecto_pytest/conftest.py
+++ proyecto_pytest/conftest.py
@@ -1,14 +1,5 @@
-from py.xml import html
-
 def pytest_html_report_title(report):
     report.title = "Reporte QA - Jenkins Visa"
-
-def pytest_html_results_summary(prefix, summary, postfix):
-    prefix.extend([
-        html.p("QA: Juan Francisco Candia"),
-        html.p("Proyecto: Jenkins Visa | Rama: develop"),
-        html.p("Fecha: 15-Dic-2025")
-    ])
 
 def pytest_configure(config):
     config._metadata['QA'] = 'Juan Francisco Candia'
